9048695f196c5ef37ca90d8dc5afc050d3e6b2b4
Marques Martins, Marcel (SFS IT PSQF BDS) <marcel.marques_martins@siemens.com>
2025-08-05 11:04:42 +0100

    fix: github actions deployment & k8s structure

--- k8s/start.sh
+++ k8s/start.sh
@@ -6,11 +6,14 @@
 #kubectl delete deployment,service,rs --all
 
 kubectl apply -f configMap.yaml
-kubectl apply -f volumes -f pods -f services
+kubectl apply -f deployment.yaml
+kubectl apply -f volumes/pv*.yaml
+kubectl apply -f kustomization.yaml
 
 helm install stable-mysql \
   --set mysqlRootPassword=root,mysqlUser=root,mysqlPassword=root,mysqlDatabase=sylius_dev,persistence.existingClaim=mysql-pvc \
   stable/mysql
 
-kubectl apply -f services/nginx.yaml
+kubectl apply -f service.yaml
+kubectl apply -f service-monitor.yaml
 
